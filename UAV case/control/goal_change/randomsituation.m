c4bc2db101bcff50a77b88d2985c4279702bc2db
luoyixingMandy <yixingluo@pku.edu.cn>
2019-12-23 22:37:23 +0800

    1223

--- UAV case/control/goal_change/randomsituation.m
+++ UAV case/control/goal_change/randomsituation.m
@@ -8,8 +8,8 @@ function [condition, index] = randomsituation(num,k)
 % disturb = randperm(7);
 % global configure
 configure = Configure();
-l = [1,2,3];
-disturb = randi([4,6],1,l(k));
+l = [2,4,6];
+disturb = randi([1,3],1,l(k));
 
 a=2:(configure.Time_target/configure.Time_step-1);
 K=randperm(length(a));
