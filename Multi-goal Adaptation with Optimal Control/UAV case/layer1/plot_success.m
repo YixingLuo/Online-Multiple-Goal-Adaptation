e5ce18bfda25c9caddad20e07e0468039143cc2f
luoyixing <luoyi_xing@126.com>
2020-02-15 17:46:12 +0800

    0215

--- Multi-goal Adaptation with Optimal Control/UAV case/layer1/plot_success.m
+++ Multi-goal Adaptation with Optimal Control/UAV case/layer1/plot_success.m
@@ -2,19 +2,20 @@ figure,
 layer1_success = [0.97,0.96,0.74,0.67,0.47];
 layer2_success = [0.99,0.99,0.98,0.98,0.98];
 k=[1,2,3,4,5];
-plot(k,layer1_success,'-^','linewidth',1.2,'Color',[0.8500 0.3250 0.0980],'MarkerFaceColor',[0.8500 0.3250 0.0980],'MarkerEdgeColor',[0.8500 0.3250 0.0980])
+plot(k,layer1_success,'-^','linewidth',2,'Color',[0.8500 0.3250 0.0980],'MarkerFaceColor',[0.8500 0.3250 0.0980],'MarkerEdgeColor',[0.8500 0.3250 0.0980])
 hold on
-plot(k,layer2_success,'--d','linewidth',1.2,'Color',[0 0.4470 0.7410],'MarkerFaceColor',[0 0.4470 0.7410],'MarkerEdgeColor',[0 0.4470 0.7410])
-
+plot(k,layer2_success,'--d','linewidth',2,'Color',[0 0.4470 0.7410],'MarkerFaceColor',[0 0.4470 0.7410],'MarkerEdgeColor',[0 0.4470 0.7410])
+set(gca,'fontname','Times' ,'FontSize',14);
 hlegend = legend('Constrained Planning','Our Approach');
-h = xlabel({'Number of soft goals'},'Fontname', 'Times New Roman');
+legend('FontName','Times New Roman','FontSize',14);
+h = xlabel({'Number of soft goals'},'Fontname', 'Times New Roman','FontSize',18);
 % xlabel({'|TG|'},'Fontname', 'Times New Roman','Interpreter', 'latex');
-ylabel('Success Probability','Fontname', 'Times New Roman');
+ylabel('Success Probability','Fontname', 'Times New Roman','FontSize',18);
 set(hlegend,'box','off')
 grid on
 axis([0, 6, 0, 1.1])
 set(gca,'gridLineStyle', '-.');
-set(gca,'fontname','Times');
+
 xlim = get(gca,'XLim');
 ylim = get(gca,'YLim');
 % set(h,'Position',[xlim(2)+(xlim(2)+xlim(1))*0.2,ylim(1)])
