b0b4811b28a3b3a3607b711dd7b3273bfbc2eb7a
Captain <luoyi_xing@126.com>
2020-08-06 16:58:42 +0800

    0806

--- Multi-goal Adaptation with Optimal Control/UAV case/Safety/plot_SR_PR2.m
+++ Multi-goal Adaptation with Optimal Control/UAV case/Safety/plot_SR_PR2.m
@@ -10,14 +10,14 @@ yy=interp1(k,SR(:,1),xx,'PCHIP');
 % yi = smooth(SR(:,1)) ;
 % h1 = plot(k,SR(:,1), '-^','linewidth',1.2,'MarkerFaceColor',[0.4940 0.1840 0.5560],'MarkerEdgeColor',[0.4940 0.1840 0.5560]);
 % hold on
-plot(k,SR(:,1),'-^','linewidth',3,'Color',[0.52941 0.80784 0.92157],'MarkerFaceColor',[0.52941 0.80784 0.92157],'MarkerEdgeColor',[0.52941 0.80784 0.92157])
+plot(k,SR(:,1),':*','MarkerSize',8,'linewidth',4,'Color',[0.52941 0.80784 0.98],'MarkerFaceColor',[0.52941 0.80784 0.98],'MarkerEdgeColor',[0.52941 0.80784 0.98])
 hold on
 
 xx=linspace(k(end),k(1));
 yy=interp1(k,SR(:,2),xx,'PCHIP');
 % h2 =plot(k,SR(:,2),'linewidth',1.2,':s','MarkerFaceColor',[0.4940 0.1840 0.5560],'MarkerEdgeColor',[0.4940 0.1840 0.5560]);
 % hold on
-plot(k,SR(:,2),':s','linewidth',2.5,'Color',[0.39216 0.58431 0.92941],'MarkerFaceColor',[0.39216 0.58431 0.92941],'MarkerEdgeColor',[0.39216 0.58431 0.92941])
+plot(k,SR(:,2),'--*','MarkerSize',8,'linewidth',4,'Color',[0 0.74 1],'MarkerFaceColor',[0 0.74 1],'MarkerEdgeColor',[0 0.74 1])
 hold on
 
 % plot(k, relax_for_52(:,3), '-','linewidth',1.2,'Color',[0.8500 0.3250 0.0980])
@@ -25,10 +25,10 @@ xx=linspace(k(end),k(1));
 yy=interp1(k,SR(:,3),xx,'PCHIP');
 % h3 = plot(k,SR(:,3),'linewidth',1.2, '--d','MarkerFaceColor',[0.4940 0.1840 0.5560],'MarkerEdgeColor',[0.4940 0.1840 0.5560]);
 % hold on
-plot(k,SR(:,3),'--d','linewidth',3,'Color',[0 0.4470 0.7410],'MarkerFaceColor',[0 0.4470 0.7410],'MarkerEdgeColor',[0 0.4470 0.7410])
+plot(k,SR(:,3),'-*','MarkerSize',8,'linewidth',4,'Color',[0 0.4470 0.7410],'MarkerFaceColor',[0 0.4470 0.7410],'MarkerEdgeColor',[0 0.4470 0.7410])
 hold on
 axis([0, 6, -2, 4])
-% hlegend = legend('AMOCS-MA','GSlack','Captain','location','northwest');
+% hlegend = legend('AMOCS-MA','GSlack','CAPTAIN','location','northwest');
 h = xlabel({'\rho_o[%]'},'Fontname', 'Times New Roman');
 % set(hlegend,'box','off')
 grid on
@@ -42,23 +42,23 @@ ylabel({'Accumulated Safety Risk (SR)'},'Fontname', 'Times New Roman','FontSize'
 
 yyaxis right
 
-plot(k,PR(:,1),'-^','linewidth',3,'Color',[1,0.62745, 0.47843],'MarkerFaceColor',[1,0.62745, 0.47843],'MarkerEdgeColor',[1,0.62745, 0.47843])
+plot(k,PR(:,1),':*','MarkerSize',8,'linewidth',4,'Color',[1,0.62745, 0.47843],'MarkerFaceColor',[1,0.62745, 0.47843],'MarkerEdgeColor',[1,0.62745, 0.47843])
 hold on
 
-plot(k,PR(:,2),':s','linewidth',2.5,'Color',[1,0.64706, 0],'MarkerFaceColor',[1,0.64706, 0],'MarkerEdgeColor',[1,0.64706, 0])
+plot(k,PR(:,2),'--*','MarkerSize',8,'linewidth',4,'Color',[1,0.64706, 0],'MarkerFaceColor',[1,0.64706, 0],'MarkerEdgeColor',[1,0.64706, 0])
 hold on
 
-plot(k,PR(:,3),'--d','linewidth',3,'Color',[1, 0.49804, 0.31373],'MarkerFaceColor',[1, 0.49804, 0.31373],'MarkerEdgeColor',[1, 0.49804, 0.31373])
+plot(k,PR(:,3),'-*','MarkerSize',8,'linewidth',4,'Color',[1, 0.388, 0.278],'MarkerFaceColor',[1, 0.388, 0.278],'MarkerEdgeColor',[1, 0.388, 0.278])
 hold on
 
-% hlegend = legend('AMOCS-MA','GSlack','Captain','location','northwest');
+% hlegend = legend('AMOCS-MA','GSlack','CAPTAIN','location','northwest');
 h = xlabel({'\rho_o[%]'},'Fontname', 'Times New Roman');
 ylabel({'Accumulated Privacy Risk (PR)'},'Fontname', 'Times New Roman','FontSize',22);
 % set(hlegend,'box','off')
 grid on
 axis([0, 6, 0, 4])
 set(gca,'ytick',0:1:4);
-hlegend = legend('AMOCS-MA(SR)','GSlack(SR)','Captain(SR)','AMOCS-MA(PR)','GSlack(PR)','Captain(PR)','location','northwest');
+hlegend = legend('AMOCS-MA(SR)','GSlack(SR)','CAPTAIN(SR)','AMOCS-MA(PR)','GSlack(PR)','CAPTAIN(PR)','location','northwest');
 set(hlegend,'box','off')
 
 % set(gca,'gridLineStyle', '-.');
@@ -77,17 +77,17 @@ yyaxis left
 xx=linspace(k2(end),k2(1));
 yy=interp1(k2,SR2(:,1),xx,'PCHIP');
 % yi = smooth(SR2(:,1)) ;
-plot(k2,SR2(:,1),'-^','linewidth',3,'Color',[0.52941 0.80784 0.92157],'MarkerFaceColor',[0.52941 0.80784 0.92157],'MarkerEdgeColor',[0.52941 0.80784 0.92157])
+plot(k2,SR2(:,1),':*','MarkerSize',8,'linewidth',4,'Color',[0.52941 0.80784 0.98],'MarkerFaceColor',[0.52941 0.80784 0.98],'MarkerEdgeColor',[0.52941 0.80784 0.98])
 hold on
 
 xx=linspace(k2(end),k2(1));
 yy=interp1(k2,SR2(:,2),xx,'PCHIP');
-plot(k2,SR2(:,2),':s','linewidth',2.5,'Color',[0.39216 0.58431 0.92941],'MarkerFaceColor',[0.39216 0.58431 0.92941],'MarkerEdgeColor',[0.39216 0.58431 0.92941])
+plot(k2,SR2(:,2),'--*','MarkerSize',8,'linewidth',4,'Color',[0 0.74 1],'MarkerFaceColor',[0 0.74 1],'MarkerEdgeColor',[0 0.74 1])
 hold on
 
 xx=linspace(k2(end),k2(1));
 yy=interp1(k2,SR2(:,3),xx,'PCHIP');
-plot(k2,SR2(:,3),'--d','linewidth',3,'Color',[0 0.4470 0.7410],'MarkerFaceColor',[0 0.4470 0.7410],'MarkerEdgeColor',[0 0.4470 0.7410])
+plot(k2,SR2(:,3),'-*','MarkerSize',8,'linewidth',4,'Color',[0 0.4470 0.7410],'MarkerFaceColor',[0 0.4470 0.7410],'MarkerEdgeColor',[0 0.4470 0.7410])
 hold on
 axis([0, 6, 0, 4])
 
@@ -104,16 +104,16 @@ ylabel({'Accumulated Safety Risk (SR)'},'Fontname', 'Times New Roman','FontSize'
 set(gca,'ytick',0:1:4);
 
 yyaxis right
-plot(k2,PR2(:,1),'-^','linewidth',3,'Color',[1,0.62745, 0.47843],'MarkerFaceColor',[1,0.62745, 0.47843],'MarkerEdgeColor',[1,0.62745, 0.47843])
+plot(k2,PR2(:,1),':*','MarkerSize',8,'linewidth',4,'Color',[1,0.62745, 0.47843],'MarkerFaceColor',[1,0.62745, 0.47843],'MarkerEdgeColor',[1,0.62745, 0.47843])
 hold on
 
-plot(k2,PR2(:,2),':s','linewidth',2.5,'Color',[1,0.64706, 0],'MarkerFaceColor',[1,0.64706, 0],'MarkerEdgeColor',[1,0.64706, 0])
+plot(k2,PR2(:,2),'--*','MarkerSize',8,'linewidth',4,'Color',[1,0.64706, 0],'MarkerFaceColor',[1,0.64706, 0],'MarkerEdgeColor',[1,0.64706, 0])
 hold on
 
-plot(k2,PR2(:,3),'--d','linewidth',3,'Color',[1, 0.49804, 0.31373],'MarkerFaceColor',[1, 0.49804, 0.31373],'MarkerEdgeColor',[1, 0.49804, 0.31373])
+plot(k2,PR2(:,3),'-*','MarkerSize',8,'linewidth',4,'Color',[1, 0.388, 0.278],'MarkerFaceColor',[1, 0.388, 0.278],'MarkerEdgeColor',[1, 0.388, 0.278])
 hold on
 
-hlegend = legend('AMOCS-MA','GSlack','Captain','location','northwest');
+hlegend = legend('AMOCS-MA','GSlack','CAPTAIN','location','northwest');
 
 h = xlabel({'\rho_c[%]'},'Fontname', 'Times New Roman');
 ylabel({'Accumulated Privacy Risk (PR)'},'Fontname', 'Times New Roman','FontSize',24);
@@ -121,7 +121,7 @@ set(hlegend,'box','off')
 grid on
 axis([0, 6, -2, 4])
 
-hlegend = legend('AMOCS-MA(SR)','GSlack(SR)','Captain(SR)','AMOCS-MA(PR)','GSlack(PR)','Captain(PR)','location','northwest');
+hlegend = legend('AMOCS-MA(SR)','GSlack(SR)','CAPTAIN(SR)','AMOCS-MA(PR)','GSlack(PR)','CAPTAIN(PR)','location','northwest');
 set(hlegend,'box','off')
 % set(gca,'gridLineStyle', '-.');
 % set(gca,'fontname','Times');
